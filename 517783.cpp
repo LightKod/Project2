2c6a51af4c9a274f609e1a853c2c22be45529527
lucky634634 <khoa21102003@gmail.com>
2022-07-05 15:31:27 +0700

    fix bugs

--- 517783.cpp
+++ 517783.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 /*
 	Dung cai cmd nay de test
-	g++ 517783.cpp | .\a.exe [may chu cai ghi lien nhau] 
+	g++ 517783.cpp | .\a.exe [may chu cai ghi lien nhau]
 	g++ 517783.cpp | .\a.exe cape
 
 	De ti chinh lai may chu cai ghi cach nhau giong trong file
@@ -14,46 +14,56 @@ using namespace std;
 	A` voi lai cai dem so tu` no bi nguoc xuong duoi, de ti xem cach chinh lai
 */
 
-struct Node {
-	Node* child[26];
+struct Node
+{
+	Node *child[26];
 	bool terminal;
 };
-int Num = 0;
 
-
-Node* CreateNode() {
-	Node* n = new Node();
-	for (int i = 0; i < 26; i++) {
+Node *CreateNode()
+{
+	Node *n = new Node();
+	for (int i = 0; i < 26; i++)
+	{
 		n->child[i] = NULL;
 	}
 	n->terminal = false;
 	return n;
 }
 
-void InsertNode(Node* &root, string key, int d = 0) {
-	if (root == NULL) root = CreateNode();
-	int value = key[d] - 'a';	
-	if (d == key.length()) {
+void InsertNode(Node *&root, string key, int d = 0)
+{
+	if (root == NULL)
+		root = CreateNode();
+	int value = key[d] - 'a';
+	if (d == key.length())
+	{
 		root->terminal = true;
 		return;
 	}
 	InsertNode(root->child[value], key, d + 1);
 }
 
-bool FindNode(Node* root, string key, int d) {
-	if (root == NULL) return false;
-	if (d == key.length()) return true;
+bool FindNode(Node *root, string key, int d)
+{
+	if (root == NULL)
+		return false;
+	if (d == key.length())
+		return true;
 	int c = key[d] - 'a';
 }
 
-
-void PrintTrie(Node* root, string s = "") {
-	if (root->terminal) {
+void PrintTrie(Node *root, string s = "")
+{
+	if (root->terminal)
+	{
 		cout << s << endl;
 	}
 
-	for (int i = 0; i < 26; i++) {
-		if (root->child[i] != NULL) {
+	for (int i = 0; i < 26; i++)
+	{
+		if (root->child[i] != NULL)
+		{
 			char c = i + 'a';
 			string newS = s + c;
 			PrintTrie(root->child[i], newS);
@@ -61,68 +71,92 @@ void PrintTrie(Node* root, string s = "") {
 	}
 }
 
-void PrintWordRecursize(Node* root, bool* check, string s = ""){
-	if (root->terminal) {
+void PrintWordRecursize(Node *root, bool *check, string s = "")
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	if (root->terminal)
+	{
 		cout << s << endl;
 	}
 
-
-	for (int i = 0; i < 26; i++) {
-		if (root->child[i] != NULL && check[i]) {
+	for (int i = 0; i < 26; i++)
+	{
+		if (root->child[i] != NULL && check[i])
+		{
 			char c = i + 'a';
 			string newS = s + c;
 			PrintWordRecursize(root->child[i], check, newS);
 		}
 	}
 }
-void CountWord(Node* root, bool* check, string s = "") {
-	if (root->terminal) {
-		Num++;
+void CountWord(Node *root, bool *check, int &num, string s = "")
+{
+	if (root == NULL)
+		return;
+	if (root->terminal)
+	{
+		num++;
 	}
 
-
-	for (int i = 0; i < 26; i++) {
-		if (root->child[i] != NULL && check[i]) {
+	for (int i = 0; i < 26; i++)
+	{
+		if (root->child[i] != NULL && check[i])
+		{
 			char c = i + 'a';
 			string newS = s + c;
-			CountWord(root->child[i], check, newS);
+			CountWord(root->child[i], check, num, newS);
 		}
 	}
 }
-void PrintAllWord(Node* root, string words){
-	bool* check = new bool[26]{false};
+void PrintAllWord(Node *root, string words)
+{
+	bool *check = new bool[26]{false};
 	int n = words.length();
 	for (int i = 0; i < n; i++)
 	{
 		check[words[i] - 'a'] = true;
 	}
-	CountWord(root, check);
+	int Num = 0;
+	CountWord(root, check, Num);
 	cout << Num << endl;
 	PrintWordRecursize(root, check);
 }
 
-void ReadDict(Node* &root, string file_name) {
+void ReadDict(Node *&root, string file_name)
+{
 	ifstream file(file_name);
+	if (!file.is_open())
+	{
+		return;
+	}
 	string s;
-	while (!file.eof()) {
-		getline(file,s);
+	while (!file.eof())
+	{
+		getline(file, s);
 		InsertNode(root, s);
 	}
 }
 
-int main(int argc, char *argv[]) {
-	Node* root = NULL;
-	ReadDict(root, "Dic.txt");
-	//PrintTrie(root);
+int main(int argc, char *argv[])
+{
+	Node *root = NULL;
+	ReadDict(root, "Dict1.txt");
+	// PrintTrie(root);ay
 
 	cout << "\n\n\n\n\n";
 	string words;
-	if(argc == 1){
+	if (argc == 1)
+	{
 		cout << "Nhap cac ki tu : ";
 		cin >> words;
-	}else{
+	}
+	else
+	{
 		words = argv[1];
 	}
 	PrintAllWord(root, words);
-
+	system("pause");
 }
